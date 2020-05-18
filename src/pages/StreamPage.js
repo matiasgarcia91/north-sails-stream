c6f3b71bb32fdde9c3411efff2db8b938b7c0530
Matias Garcia <matiasigarcia91@gmail.com>
2020-05-18 18:48:44 +0200

    add test youtube video

---
+++ src/pages/StreamPage.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const StreamPage = () => {
+  return (
+    <div>
+      <iframe
+        title='north stream'
+        width='724'
+        height='568'
+        src='https://www.youtube.com/embed/jm-wsyJ6TwA'
+        frameborder='0'
+        allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+        allowfullscreen
+      ></iframe>
+    </div>
+  );
+};
+
+export default StreamPage;
