9ee31d3542617f09e164f4df2248513ddf4eca03
Matias Garcia <matiasigarcia91@gmail.com>
2020-06-21 14:00:20 +0200

    style login page

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -2,13 +2,28 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { login } from "../store/user/actions";
 import { useHistory } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Paper from "@material-ui/core/Paper";
 import "./login.css";
+
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: "rgba(255,246,240,0.8)",
+    padding: "50px 30px 30px 30px",
+    marginBottom: 200,
+  },
+});
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const dispatch = useDispatch();
   const history = useHistory();
+
+  const classes = useStyles();
 
   const submitLogin = e => {
     e.preventDefault();
@@ -19,30 +34,47 @@ const LoginPage = () => {
 
   return (
     <div className='login'>
-      <form
-        style={{ display: "flex", flexDirection: "column" }}
-        onSubmit={submitLogin}
+      <Paper
+        elevation={3}
+        classes={{
+          root: classes.root,
+        }}
       >
-        <div>
-          <label>Email</label>
-          <input
-            name='email'
-            type='email'
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>Password</label>
-          <input
-            name='password'
-            type='password'
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-          />
-        </div>
-        <button type='submit'>Submit</button>
-      </form>
+        <form
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
+          onSubmit={submitLogin}
+        >
+          <div className='form-fields'>
+            <TextField
+              variant='filled'
+              name='email'
+              label='Email'
+              type='email'
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+          </div>
+          <div className='form-fields'>
+            <TextField
+              variant='filled'
+              name='password'
+              label='Password'
+              type='password'
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+          </div>
+          <div className='form-button'>
+            <Button variant='contained' color='primary' type='submit'>
+              Login
+            </Button>
+          </div>
+        </form>
+      </Paper>
     </div>
   );
 };
