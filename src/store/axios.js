6361201eba04b6d777f36824bfad213a3f8d6a89
Matias Garcia <matiasigarcia91@gmail.com>
2020-06-24 23:58:12 +0200

    override vimeo fullscreen with own, add overlay

--- src/store/axios.js
+++ src/store/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { HEROKU_URL } from "../constants";
+import { API_URL } from "../constants";
 export default axios.create({
-  baseURL: HEROKU_URL, //"https://north-sails-server.herokuapp.com",
+  baseURL: API_URL, //"https://north-sails-server.herokuapp.com",
 });
