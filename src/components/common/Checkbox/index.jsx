3cce5cb2e9d89a44414d2f4dda45c7a6c4cc05b7
avizmarques <aviz.marques@gmail.com>
2021-10-30 21:45:27 +0200

    styling upload accounts modal

--- src/components/common/Checkbox/index.jsx
+++ src/components/common/Checkbox/index.jsx
@@ -1,20 +1,28 @@
 import React from "react";
 import styled from "styled-components";
+import { variant } from "styled-system";
+
+const variants = {
+  primary: { height: "32px" },
+  small: { height: "16px" },
+};
 
 const StyledInput = styled.input`
   width: 100%;
   padding: 8px;
-  height: 32px;
   max-height: 32px;
   border-radius: 6px;
-  border: 1px solid ${p => p.theme.colors.grey200};
+  border: 1px solid ${(p) => p.theme.colors.grey200};
   background: #faf9fd;
   font-family: inherit;
+
   &:hover,
   &:focus {
-    border: 1px solid ${p => p.theme.colors.primary};
+    border: 1px solid ${(p) => p.theme.colors.primary};
     outline: none;
   }
+
+  ${variant({ variants })}
 `;
 
 const StyledDiv = styled.div`
@@ -25,7 +33,7 @@ const StyledDiv = styled.div`
 
 const StyledLabel = styled.label`
   font-weight: 700;
-  color: ${p => p.theme.colors.grey400};
+  color: ${(p) => p.theme.colors.grey400};
   text-align: left;
   font-size: 16px;
 `;
@@ -36,8 +44,14 @@ export const Checkbox = ({ children, variant = "primary", ...props }) => {
       <div>
         <StyledLabel>{props.label}</StyledLabel>
       </div>
-      <StyledDiv style={{ maxHeight: 32, minWidth: 28, marginLeft: 20 }}>
-        <StyledInput variant={variant} {...props} type='checkbox'>
+      <StyledDiv
+        style={{
+          maxHeight: 32,
+          minWidth: 28,
+          marginLeft: variant === "primary" && 20,
+        }}
+      >
+        <StyledInput variant={variant} {...props} type="checkbox">
           {/* <Icon as="span" className="button-background" icon={ButtonIcon} /> */}
           {children}
         </StyledInput>
